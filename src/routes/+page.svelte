197bef84a46535a452a7df9c2018dcd2acbb14bb
James Hannan <jim9112@gmail.com>
2024-11-10 18:59:36 -0500

    Add hero on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
-<h1 class="text-3xl">Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
-</p>
+<div class="hero bg-base-200 min-h-screen">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Hello there</h1>
+      <p class="py-6">
+        My name is James and I enjoy helping businesses thrive on the web.
+      </p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
