5bdbaecf0f78cc9fcdf38fc1c2b622ebabf77abb
James Hannan <jim9112@gmail.com>
2024-11-10 18:10:22 -0500

    install daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  daisyui: {
+    themes: [
+      "dracula"
+    ],
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 }
 
